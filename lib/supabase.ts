86435961203637fde8e24a6e975461805f20eabd
Matthew Yin <39784530+mlyin@users.noreply.github.com>
2026-09-17 21:09:44 -0400

    Build Fantasy Market MVP

---
+++ lib/supabase.ts
@@ -0,0 +1,2 @@
+import { createClient } from '@supabase/supabase-js';
+export const supabase=createClient('https://uwpqafsfmtzowqlynlqf.supabase.co','sb_publishable_bzP92k3SqMMwQgJQIIkrqw_6zUuaq_Q');
